0cedac95d1c607961f68ef2200fc4fc9f1107483
BlueWolf <andrisdeak2006@gmail.com>
2026-01-16 11:24:01 +0100

    Javascript cleanup

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 let fileInput = document.getElementById('upload-btn');
-fileInput.onchange = () => {
-    let selectedFile = fileInput.files[0];
-    const reader = new FileReader();
 
-    reader.onload = function (file) {
-        let text = file.target.result;
-        //const table = document.getElementById("stattable");
-        let nccp = parseInt(text.match(/NominalChargeCapacity":\s*([0-9.]+)/i)[1]) / parseInt(text.match(/last_value_MaximumFCC":\s*([0-9.]+)/i)[1]) * 100;
-        document.getElementById("rbh").getElementsByTagName("td")[1].textContent = text.match(/last_value_MaximumCapacityPercent":\s*([0-9.]+)/i)[1] + "%";
-        //rightalign todo
-        document.getElementById("cc").getElementsByTagName("td")[1].textContent = text.match(/last_value_CycleCount":\s*([0-9.]+)/i)[1];
-        document.getElementById("cbh").getElementsByTagName("td")[1].textContent = nccp.toFixed(2) + "%";
-        document.getElementById("minfcc").getElementsByTagName("td")[1].textContent = text.match(/last_value_MinimumFCC":\s*([0-9.]+)/i)[1] + " mAh";
-        document.getElementById("maxfcc").getElementsByTagName("td")[1].textContent = text.match(/last_value_MaximumFCC":\s*([0-9.]+)/i)[1] + " mAh";
-        document.getElementById("ncc").getElementsByTagName("td")[1].textContent = text.match(/NominalChargeCapacity":\s*([0-9.]+)/i)[1] + " mAh";
-        document.getElementById("at").getElementsByTagName("td")[1].textContent = text.match(/AverageTemperature":\s*([0-9.]+)/i)[1] + " °C";
-    };
-    reader.readAsText(selectedFile);
-}
+const reader = new FileReader();
+
+reader.onload = function (file) {
+    let text = file.target.result;
+    let nccp = parseInt(text.match(/NominalChargeCapacity":\s*([0-9.]+)/i)[1]) / parseInt(text.match(/last_value_MaximumFCC":\s*([0-9.]+)/i)[1]) * 100;
+    document.getElementById("rbh").getElementsByTagName("td")[1].textContent = text.match(/last_value_MaximumCapacityPercent":\s*([0-9.]+)/i)[1] + "%";
+    document.getElementById("cc").getElementsByTagName("td")[1].textContent = text.match(/last_value_CycleCount":\s*([0-9.]+)/i)[1];
+    document.getElementById("cbh").getElementsByTagName("td")[1].textContent = nccp.toFixed(2) + "%";
+    document.getElementById("minfcc").getElementsByTagName("td")[1].textContent = text.match(/last_value_MinimumFCC":\s*([0-9.]+)/i)[1] + " mAh";
+    document.getElementById("maxfcc").getElementsByTagName("td")[1].textContent = text.match(/last_value_MaximumFCC":\s*([0-9.]+)/i)[1] + " mAh";
+    document.getElementById("ncc").getElementsByTagName("td")[1].textContent = text.match(/NominalChargeCapacity":\s*([0-9.]+)/i)[1] + " mAh";
+    document.getElementById("at").getElementsByTagName("td")[1].textContent = text.match(/AverageTemperature":\s*([0-9.]+)/i)[1] + " °C";
+};
+
+fileInput.onchange = () => {
+    reader.readAsText(fileInput.files[0]);
+};
